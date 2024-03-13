527047b74085a2a4e87c2a3c3bdc9da10ef8604e
Termux and Linux <linuxundtermux@gmail.com>
2024-03-13 01:56:36 +0100

    update

--- ubuntu/main.sh
+++ ubuntu/main.sh
@@ -8,16 +8,19 @@ elif [ "$1" == "--install" ]; then
     proot-distro install ubuntu
 elif [ "$1" == "--delete" ]; then
     proot-distro remove ubuntu
+elif [ "$1" == "--gui"]; then
+    proot-distro login ubuntu --shared-tmp
 elif [ "$1" == "--delete--cmd" ]; then
     rm /data/data/com.termux/files/usr/bin/ubuntu
 elif [ "$1" == "--help" ]; then
     echo "Usage:"
-    echo "  ubuntu                 : Start proot-distro login ubuntu"
+    echo "  ubuntu                 : Login
     echo "  ubuntu --setup         : Install proot-distro"
     echo "  ubuntu --install       : Install Ubuntu"
     echo "  ubuntu --delete        : Remove Ubuntu"
     echo "  ubuntu --delete--cmd   : Remove the ubuntu command"
     echo "  ubuntu --help          : Show this help message"
+    echo " ubuntu --gui              Start GUI.in ubuntu
 else
     echo "Invalid argument. Use 'ubuntu --help' for usage instructions."
 fi
